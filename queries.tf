cd44cbb59c6e240af8f838f57147e95a99220532
Chris Gilmer <chris.gilmer@gmail.com>
2021-04-08 16:37:22 -0700

    Allow setting db and table name for existing data

--- queries.tf
+++ queries.tf
@@ -2,14 +2,14 @@
 # Queries copied from https://aws.amazon.com/premiumsupport/knowledge-center/analyze-logs-athena/
 
 locals {
-  queries = [
+  queries = compact([
     "actions_by_user",
-    "create_table",
+    length(var.database_name) > 0 ? "" : "create_table",
     "log_deleted_object",
     "operations_on_object",
     "show_data_transfer_by_ip",
     "who_deleted_object",
-  ]
+  ])
 }
 
 data "template_file" "query" {
@@ -18,7 +18,7 @@ data "template_file" "query" {
   template = file("${path.module}/query_templates/${local.queries[count.index]}.tmpl")
   vars = {
     account_id              = data.aws_caller_identity.current.account_id
-    database_and_table_name = format("%s.%s", local.db_name, local.db_name)
+    database_and_table_name = format("%s.%s", local.db_name, local.table_name)
     partition               = data.aws_partition.current.partition
     target_location         = format("%s/%s", var.target_bucket, var.target_prefix)
   }
@@ -31,6 +31,6 @@ resource "aws_athena_named_query" "query" {
   name        = local.queries[count.index]
   description = format("%s for querying s3 access logs", local.queries[count.index])
   workgroup   = module.athena_workgroup.id
-  database    = aws_athena_database.access_logs.name
+  database    = length(var.database_name) > 0 ? local.db_name : aws_athena_database.access_logs[0].name
   query       = data.template_file.query[count.index].rendered
 }
